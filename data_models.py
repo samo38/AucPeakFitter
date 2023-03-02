cf9e42a5c76c8a4c05792eb0cc358aa1029e49af
samo38 <saeed.mortezazadeh25@gmail.com>
2023-03-02 11:44:11 -0700

    add enums, add Data class

--- data_models.py
+++ data_models.py
@@ -1,4 +1,11 @@
 import numpy as np
+import enum
+
+
+class Types(enum.Enum):
+    EMPTY = 0
+    GAUSS = 1
+    EXP = 2
 
 
 class Params:
@@ -28,26 +35,27 @@ class Exponential:
 
 class Model:
 
-    def __init__(self, t: str):
-        if t == "GAUSS":
-            self.type = t
+    def __init__(self, t: enum.Enum):
+        if t == Types.EMPTY:
+            self.params = None
+        elif t == Types.GAUSS:
             self.params = Gaussian()
-        elif t == "EXP":
-            self.type = t
+        elif t == Types.EXP:
             self.params = Exponential()
+        else:
+            raise IOError("input value not exist!")
+        self.type = t
         self.name = ''
         self.visible = True
 
 
-class DataModels:
+class Data:
 
     def __init__(self):
         self.x_raw = None
         self.y_raw = None
         self.x_trim = None
         self.y_trim = None
-        self.models = []
-        self.optimized = False
 
     def set_raw(self, x: np.array, y: np.array):
         self.x_raw = x
@@ -57,18 +65,31 @@ class DataModels:
         self.x_trim = x
         self.y_trim = y
 
+
+class DataModels:
+
+    def __init__(self):
+        self.data = Data()
+        self.models = []
+        self.optimized = False
+
     def append_model(self, model: Model):
-        if model.type is not None:
+        if model.type != Types.EMPTY:
             self.models.append(model)
 
     def set_model(self, n: int, model: Model):
-        if model.type is not None:
+        if model.type != Types.EMPTY:
             self.models[n] = model
 
+    def get_model(self, n: int):
+        return self.models[n]
+
+    def set_data(self, data: Data):
+        self.data = data
+
     def get_data(self):
-        return self.x_raw, self.y_raw, self.x_trim, self.y_trim
+        return self.data
 
     def clear(self):
-        self.x_trim = None
-        self.y_trim = None
+        self.data = Data()
         self.models = []
